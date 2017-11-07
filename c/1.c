3aa66da95dcc86557c8155d5191b0b4fd9c098ea
scottf <this.specific.scott@gmail.com>
2017-11-07 14:17:14 -0500

    added gitignore for compiled c files

---
+++ c/1.c
@@ -0,0 +1,15 @@
+// Find the sum of all the multiples of 3 or 5 below 1000.
+#include <stdio.h>
+
+int main()
+{
+	int sum = 0;
+
+	for (int i = 0; i < 1000; ++i)
+	{
+		if (((i % 3) == 0) || ((i % 5) == 0))
+			sum = i + sum;
+	}
+
+	printf("%d\n", sum); // 233168
+}
