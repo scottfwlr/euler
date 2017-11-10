38e0f3c0b44985aa7c60560c4494263dbb118172
scottf <this.specific.scott@gmail.com>
2017-11-10 10:32:06 -0500

    completed 3 in C

---
+++ c/3.c
@@ -0,0 +1,32 @@
+// What is the largest prime factor of the number 600851475143
+
+#include <stdio.h>
+
+int factorise(long n);
+
+int main() 
+{
+	int res;
+	long num = 600851475143;
+
+	res = factorise(num);
+	printf("%d\n", res);
+	return 0;
+}
+
+int factorise(long n) {
+	int div, factor;
+
+	div = 2; // smallest prime
+
+	while (n > 1)
+	{
+		while (n % div == 0)
+		{
+			factor = div; // last is expected to be largest...
+			n = n / div; 
+		}
+		div++; // ... because we are strictly incrementing
+	}
+	return factor; // 6857
+}
