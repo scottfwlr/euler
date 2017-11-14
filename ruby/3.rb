8870d7d665665110f9e3b4410dbe9b0cdb5a3265
scottf <this.specific.scott@gmail.com>
2017-11-13 20:41:20 -0500

    completed ruby 3

--- ruby/3.rb
+++ ruby/3.rb
@@ -1,13 +1,29 @@
 # What is the largest prime factor of the number 600851475143?
 
-def not_prime?(n)
-  (2..Math.sqrt(n).ceil).any? { |e| n % e == 0 }
+def factorise(n)
+  case n
+  when 3
+    [1, 3]
+  when 5
+    [1, 5]
+  when 7
+    [1, 7]
+  else
+    d = (2..Math.sqrt(n).ceil).find { |e| n % e == 0 }
+    d = n if d.nil?
+    [n/d, d]
+  end
 end
 
 def lpf(n)
-  factors = [n]
-  while factors.any(&:not_prime?)
-    subject = factors.delete_at(factors.index(&:not_prime?))
-    
+  composites = [n]
+  factors = []
+  until composites.empty?
+    new_n, d = factorise(composites.pop)
+    factors << d
+    composites << new_n unless new_n == 1
   end
+  factors.last
 end
+
+puts lpf(600851475143) # 6857
