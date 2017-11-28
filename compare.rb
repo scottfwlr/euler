b357e60dd229f43c880a894111c6fe87cfb7dbf8
scottf <this.specific.scott@gmail.com>
2017-11-28 18:18:15 -0500

    This is a dead end. I need to rejig the files to have a common structure instead of trying to parse everything.

--- compare.rb
+++ compare.rb
@@ -1,13 +1,33 @@
-# 
 arr = {}
 
-Dir.glob('*/') { |folder|
-  Dir.glob("#{folder}*").reject { |e| 
-    # filter out extraneous c and ruby files 
-    e[/Gemfile/] or e[/\.out/] 
-  }.each do |file|
-    
+# Goals: first 100
+1.upto(100) { |n| arr[n] = {} }
+
+def exercise_template(str)
+  """
+  **Problem #{num}:** #{instructions}
+  """
+end
+
+def parse_file(f, num, lang)
+  file = File.read(f)
+  comment = case lang
+  when 'c', 'js'
+    '//'
+  when 'rb', 'exs'
+    '#'
   end
 
+  
+end
 
-}
+Dir.glob('*/') do |folder|
+  Dir.glob("#{folder}*").reject { |e| 
+    # filter out extraneous c and ruby files 
+    e[/\.out/] or e[/hello.c/] or e[/bin/] or e[/Gemfile/] 
+  }.each do |file|
+    num, lang = file[/\/\d+\.w+/].split('.')
+    parse_file(file, num, lang)
+
+  end
+end
