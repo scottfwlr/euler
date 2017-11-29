a452174f6c85922d297c9318121a57812e38e1a3
scottf <this.specific.scott@gmail.com>
2017-11-30 10:57:30 +1300

    stumped on racket number 2

--- racket/2.rkt
+++ racket/2.rkt
@@ -1,11 +1,21 @@
 ; By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms. <<<--->>>
 
-(define (fib x)
-  (cond ((= 1 x) 1)
-        ((= 2 x) 2)
-        (else
-          ((fib (- x 1)) (fib (- x 2))))))
+(define (fib lst n) ; n for the 'n'th in fib sequence
+  (if (= n (length lst))
+    lst
+    (fib (cons (+ (first lst) (second lst)) lst) n)))
+
+
+(define (second lst)
+  (last (take lst 2)))
 
 (define (even? x)
   (= 0 (modulo x 2)))
 
+(define (below-4m? x)
+  (< x 4000000))
+
+(println
+  (foldl + 0 (filter (and even? below-4m?) (fib (list 2 1) 100)))
+  ) ; 9227463
+; something is very wrong here
