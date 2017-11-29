5a70071c6609627b7a5754a6b2c7298e5d619ab2
scottf <this.specific.scott@gmail.com>
2017-11-29 11:01:10 -0500

    halfway thru 2, plane landing

--- racket/2.rkt
+++ racket/2.rkt
@@ -1,2 +1,11 @@
 ; By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms. <<<--->>>
 
+(define (fib x)
+  (cond ((= 1 x) 1)
+        ((= 2 x) 2)
+        (else
+          ((fib (- x 1)) (fib (- x 2))))))
+
+(define (even? x)
+  (= 0 (modulo x 2)))
+
